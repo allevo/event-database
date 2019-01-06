a213b6ab91b79ad40bb0489051a6685162608bf0
Tommaso Allevi <tommaso.allevi@mia-platform.eu>
2019-01-07 00:57:01 +0100

    Travis #6

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ make
 make check
 # a reducer example
 cd ../event-database-example
-gcc -I"../event-database-sdk" -I"../jansson/src" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"counter.d" -MT"counter.o" -o "counter.o" "counter.c"
-gcc -L"../jansson/src" -L"../jansson/src/.libs" -shared -o "libevent-database-example.so"  ./counter.o   -ljansson
+gcc -I"../event-database-sdk" -I"../jansson/src" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"counter.d" -MT"counter.o" -o "counter.o" "counter.c" -std=c99
+gcc -L"../jansson/src" -L"../jansson/src/.libs" -shared -o "libevent-database-example.so"  ./counter.o   -ljansson -std=c99
 # the database
 cd ../event-database
-gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/pipe/pipe.d" -MT"src/pipe/pipe.o" -o "src/pipe/pipe.o" "src/pipe/pipe.c"
-gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/logger/log.d" -MT"src/logger/log.o" -o "src/logger/log.o" "src/logger/log.c"
-gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/event-database.d" -MT"src/event-database.o" -o "src/event-database.o" "src/event-database.c"
-gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/event-engine.d" -MT"src/event-engine.o" -o "src/event-engine.o" "src/event-engine.c"
-gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/event-storage.d" -MT"src/event-storage.o" -o "src/event-storage.o" "src/event-storage.c"
+gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/pipe/pipe.d" -MT"src/pipe/pipe.o" -o "src/pipe/pipe.o" "src/pipe/pipe.c" -std=c99
+gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/logger/log.d" -MT"src/logger/log.o" -o "src/logger/log.o" "src/logger/log.c" -std=c99
+gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/event-database.d" -MT"src/event-database.o" -o "src/event-database.o" "src/event-database.c" -std=c99
+gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/event-engine.d" -MT"src/event-engine.o" -o "src/event-engine.o" "src/event-engine.c" -std=c99
+gcc -I"../event-database-sdk" -I"../jansson/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/event-storage.d" -MT"src/event-storage.o" -o "src/event-storage.o" "src/event-storage.c" -std=c99
 gcc -L"../jansson/src/.libs" -o "event-database"  ./src/pipe/pipe.o  ./src/logger/log.o  ./src/event-database.o ./src/event-engine.o ./src/event-storage.o   -lpthread -ldl -ljansson
 
